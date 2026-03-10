c7c7c7b3b2ceff966cb46c46313e31f1c9658362
chemboliroy <chemboliroy@go-groups.com>
2026-03-10 15:02:11 +0100

    Initializing The Backend

---
+++ Backend/fee-payment/src/main/java/com/leedtechs/fee_payment/FeePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.leedtechs.fee_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeePaymentApplication.class, args);
+	}
+
+}
---
+++ Backend/fee-payment/src/test/java/com/leedtechs/fee_payment/FeePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leedtechs.fee_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
